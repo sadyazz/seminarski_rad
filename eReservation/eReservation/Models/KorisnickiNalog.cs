3b9a9ca72e39fbea5e6c27554c350c77f51ee051
sadyazz <97617659+sadyazz@users.noreply.github.com>
2024-04-28 18:44:15 +0200

    added KorisnickiNalog.cs

---
+++ eReservation/eReservation/Models/KorisnickiNalog.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace eReservation.Models
+{
+    [Table("KorisnickiNalog")]
+    public class KorisnickiNalog
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Username { get; set; }
+        [JsonIgnore]
+        public string Password { get; set; }
+
+        [JsonIgnore]
+        public User? Korisnik => this as User;
+
+        [JsonIgnore]
+        public Admin? Administrator => this as Admin;
+
+        public bool isAdministrator => Administrator != null;
+        public bool isKorisnik => Korisnik != null;
+    }
+}

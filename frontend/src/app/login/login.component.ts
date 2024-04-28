4db036eaf79413c5bf300f63942e2fb3b21be47a
sadyazz <97617659+sadyazz@users.noreply.github.com>
2024-04-28 18:45:54 +0200

    updated the login component

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthLoginRequest} from "./authLoginRequest";
+import {MojConfig} from "../moj-config";
+import {HttpClient} from "@angular/common/http";
+import {AuthLoginResponse} from "./authLoginResponse";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
 
+  public loginRequest: AuthLoginRequest = {
+    lozinka : "",
+    korisnickoIme:""
+  };
+  constructor(public httpClient:HttpClient, private router: Router) {
+  }
+
+  ngOnInit(){
+
+  }
+
+  signIn(){
+   let url = MojConfig.adresa_servera + "/Autentifikacija/login";
+    this.httpClient.post<AuthLoginResponse>(url, this.loginRequest).subscribe((x)=>{
+      if(!x.isLogiran){
+        alert("pogresan username / password")
+      }else{
+        let token=x.autentifikacijaToken.vrijednost;
+        window.localStorage.setItem("my-auth-token", token);
+        this.router.navigate(["/home"])
+      }
+    })
+  }
 }

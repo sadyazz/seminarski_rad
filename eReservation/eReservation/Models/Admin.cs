67e23d720f43fe73c23426b257424c8e17622e2b
sadyazz <97617659+sadyazz@users.noreply.github.com>
2024-04-29 14:19:09 +0200

    updated the admin and user

--- eReservation/eReservation/Models/Admin.cs
+++ eReservation/eReservation/Models/Admin.cs
@@ -1,12 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace eReservation.Models
 {
+    [Table("Admin")]
     public class Admin:KorisnickiNalog
     {
         [Key]
         public int ID { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
     }
 }

581c3d8c2f6353dd653fd3b20a3118982679461a
sadyazz <97617659+sadyazz@users.noreply.github.com>
2024-05-02 21:21:16 +0200

    added logout

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {MojConfig} from "../moj-config";
+import {HttpClient} from "@angular/common/http";
+import {MyAuthService} from "../services/MyAuthService";
+
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  constructor(public router: Router, private httpClient: HttpClient, public myAuthService: MyAuthService) {
+  }
+  ngOnInit(): void {
+
+  }
   showDropdown: boolean = false;
+
 
   toggleDropdown() {
     this.showDropdown = !this.showDropdown;
@@ -30,4 +42,24 @@ export class HomeComponent {
     if(this.brojacC>0)
       this.brojacC--;
   }
+
+  logOut() {
+    /*window.localStorage.setItem("my-auth-token","");
+    this.router.navigate(["/login"]);*/
+
+    let token = window.localStorage.getItem("my-auth-token")??"";
+    window.localStorage.setItem("my-auth-token","");
+
+    let url=MojConfig.adresa_servera+`/Autentifikacija/logout`
+    this.httpClient.post(url, {}, {
+      headers:{
+        "my-auth-token": token
+      }
+    }).subscribe(x=>{
+      console.log("logout uspjesan")
+    })
+
+    this.router.navigate(["/login"])
+  }
+
 }

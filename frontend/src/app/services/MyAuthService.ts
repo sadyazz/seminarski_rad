8fc5870377cb6962a1cf10d6906aefcbb52f1e92
sadyazz <97617659+sadyazz@users.noreply.github.com>
2024-05-08 22:11:08 +0200

    updated MyAuthService.ts

--- frontend/src/app/services/MyAuthService.ts
+++ frontend/src/app/services/MyAuthService.ts
@@ -1,13 +1,42 @@
 import {HttpClient} from "@angular/common/http";
 import {Injectable} from "@angular/core";
+import {AutentifikacijaToken} from "../../helpers/auth/autentifikacijaToken";
+
 
 @Injectable({providedIn: 'root'})
 export class MyAuthService{
   constructor(private httpClient: HttpClient) {
   }
   jelLogiran():boolean{
-    let token = window.localStorage.getItem("my-auth-token");
+    return this.getAuthorizationToken() != null;
+  }
+  getAuthorizationToken(): AutentifikacijaToken | null{
+    let tokenString = window.localStorage.getItem("my-auth-token")??"";
+    try {
+      return JSON.parse(tokenString);
+    }
+    catch (e){
+      return null;
+    }
+  }
 
-    return token != "";
+  isAdmin():boolean{
+    return this.getAuthorizationToken()?.korisnickiNalog.isAdmin ?? false
+  }
+
+  isUser():boolean{
+    return this.getAuthorizationToken()?.korisnickiNalog.isUser ?? false
+  }
+
+ /* returnId() {
+    return this.getAuthorizationToken()?.korisnickiNalog.id;
+  }*/
+  setLogiraniKorisnik(x: AutentifikacijaToken | null) {
+    if(x == null){
+      window.localStorage.setItem("my-auth-token", '');
+    }else{
+      window.localStorage.setItem("my-auth-token", JSON.stringify(x));
+    }
+
   }
 }

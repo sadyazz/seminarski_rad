2d1285493aa4f0a02e339b9e51c76acf7f04843f
sadyazz <97617659+sadyazz@users.noreply.github.com>
2024-05-08 22:11:56 +0200

    added autorizacija-guard.service.ts

---
+++ frontend/src/helpers/auth/autorizacija-guard.service.ts
@@ -0,0 +1,32 @@
+import {Injectable} from "@angular/core";
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from "@angular/router";
+import {MyAuthService} from "../../app/services/MyAuthService";
+
+
+@Injectable()
+export class AutorizacijaGuard implements CanActivate {
+
+  constructor(private router: Router, private myAuthService: MyAuthService) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+
+
+    if (this.myAuthService.jelLogiran()) {
+
+      let isUser = this.myAuthService.isUser();
+
+      if (isUser)
+      {
+        this.router.navigate(['/login']);
+        return false;
+      }
+
+      return true;
+    }
+
+
+    // not logged in so redirect to login page with the return url
+    this.router.navigate(['/login'], { queryParams: { povratniUrl: state.url }});
+    return false;
+  }
+}

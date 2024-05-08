f357e91235ffb38d751cef5a7b6875cc72f24431
sadyazz <97617659+sadyazz@users.noreply.github.com>
2024-05-08 22:10:36 +0200

    updated app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
-import {HttpClientModule} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {RouterModule} from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import {AuthInterceptor} from "../helpers/auth/auth-interceptor.service";
+import {AutorizacijaGuard} from "../helpers/auth/autorizacija-guard.service";
 
 @NgModule({
   declarations: [
@@ -25,12 +27,14 @@ import { UserProfileComponent } from './user-profile/user-profile.component';
     RouterModule.forRoot([{ path: '', component: HomeComponent },
       {path: 'login', component: LoginComponent},
       {path: 'home', component: HomeComponent},
-      {path: 'profile', component: UserProfileComponent}
+      {path: 'profile', component: UserProfileComponent, canActivate:[AutorizacijaGuard]}
     ]),
     AppRoutingModule
   ],
   providers: [
-    provideClientHydration()
+    //provideClientHydration()
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
+    AutorizacijaGuard
   ],
   bootstrap: [AppComponent]
 })

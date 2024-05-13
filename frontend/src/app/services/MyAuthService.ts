f43707137d9502d738e01d8ef5ea1b2abbc6dedb
sadyazz <97617659+sadyazz@users.noreply.github.com>
2024-05-13 21:34:11 +0200

    updated MyAuthService.ts

--- frontend/src/app/services/MyAuthService.ts
+++ frontend/src/app/services/MyAuthService.ts
@@ -2,16 +2,30 @@ import {HttpClient} from "@angular/common/http";
 import {Injectable} from "@angular/core";
 import {AutentifikacijaToken} from "../../helpers/auth/autentifikacijaToken";
 
-
 @Injectable({providedIn: 'root'})
 export class MyAuthService{
   constructor(private httpClient: HttpClient) {
   }
+
+  private getLocalStorageItem(key: string): string | null {
+    if (typeof window !== 'undefined') {
+      return window.localStorage.getItem(key) ?? null;
+    }
+    return null;
+  }
+
+  private setLocalStorageItem(key: string, value: string) {
+    if (typeof window !== 'undefined') {
+      window.localStorage.setItem(key, value);
+    }
+  }
+
   jelLogiran():boolean{
     return this.getAuthorizationToken() != null;
   }
   getAuthorizationToken(): AutentifikacijaToken | null{
-    let tokenString = window.localStorage.getItem("my-auth-token")??"";
+    //let tokenString = window.localStorage.getItem("my-auth-token")??"";
+    let tokenString = this.getLocalStorageItem("my-auth-token")??"";
     try {
       return JSON.parse(tokenString);
     }
@@ -28,14 +42,13 @@ export class MyAuthService{
     return this.getAuthorizationToken()?.korisnickiNalog.isUser ?? false
   }
 
- /* returnId() {
-    return this.getAuthorizationToken()?.korisnickiNalog.id;
-  }*/
   setLogiraniKorisnik(x: AutentifikacijaToken | null) {
     if(x == null){
-      window.localStorage.setItem("my-auth-token", '');
+      //window.localStorage.setItem("my-auth-token", '');
+      this.setLocalStorageItem("my-auth-token",'');
     }else{
-      window.localStorage.setItem("my-auth-token", JSON.stringify(x));
+      //window.localStorage.setItem("my-auth-token", JSON.stringify(x));
+      this.setLocalStorageItem("my-auth-token", JSON.stringify(x));
     }
 
   }

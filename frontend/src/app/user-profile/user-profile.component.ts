852ebd995c000da527d48cf55636d183a2d9ede8
sadyazz <97617659+sadyazz@users.noreply.github.com>
2024-05-14 21:50:02 +0200

    updated user-profile component

--- frontend/src/app/user-profile/user-profile.component.ts
+++ frontend/src/app/user-profile/user-profile.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MyAuthService} from "../services/MyAuthService";
+import {Router} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
+import {GetUserInfo, GetUserResponse} from "./get-user-info";
 
 @Component({
-  selector: 'app-user-profile',
+  selector: 'user-profile',
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements OnInit{
+
+  constructor(private myAuthService: MyAuthService, public router :Router, private httpClient: HttpClient) {
+  }
+
+
+  ngOnInit() {
+
+
+  }
 
 }

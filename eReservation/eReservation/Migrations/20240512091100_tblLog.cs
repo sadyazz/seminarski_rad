9a5ac5a6e08cfda97987c651963a977f890877d0
sadyazz <97617659+sadyazz@users.noreply.github.com>
2024-05-15 21:50:07 +0200

    new migration

---
+++ eReservation/eReservation/Migrations/20240512091100_tblLog.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eReservation.Migrations
+{
+    /// <inheritdoc />
+    public partial class tblLog : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Is2FActive",
+                table: "KorisnickiNalog",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Is2FOtkljucano",
+                table: "AutentifikacijaToken",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TwoFKey",
+                table: "AutentifikacijaToken",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "LogKretanjePoSistemu",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserID = table.Column<int>(type: "int", nullable: false),
+                    QueryPath = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PostData = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Time = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IpAddress = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ExceptionMessage = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsException = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LogKretanjePoSistemu", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_LogKretanjePoSistemu_KorisnickiNalog_UserID",
+                        column: x => x.UserID,
+                        principalTable: "KorisnickiNalog",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LogKretanjePoSistemu_UserID",
+                table: "LogKretanjePoSistemu",
+                column: "UserID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LogKretanjePoSistemu");
+
+            migrationBuilder.DropColumn(
+                name: "Is2FActive",
+                table: "KorisnickiNalog");
+
+            migrationBuilder.DropColumn(
+                name: "Is2FOtkljucano",
+                table: "AutentifikacijaToken");
+
+            migrationBuilder.DropColumn(
+                name: "TwoFKey",
+                table: "AutentifikacijaToken");
+        }
+    }
+}

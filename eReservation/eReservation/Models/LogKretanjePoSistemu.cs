ff4333074059f1b603104f4ce98e9d729dba313f
sadyazz <97617659+sadyazz@users.noreply.github.com>
2024-05-15 21:49:23 +0200

    added LogKretanjePoSistemu.cs

---
+++ eReservation/eReservation/Models/LogKretanjePoSistemu.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace eReservation.Models
+{
+    public class LogKretanjePoSistemu
+    {
+        [Key]
+        public int ID { get; set; }
+        [ForeignKey(nameof(User))]
+        public int UserID { get; set; }
+        public KorisnickiNalog User { get; set; }
+        public string? QueryPath { get; set; }
+        public string? PostData { get; set; }
+        public DateTime Time { get; set; }
+        public string? IpAddress { get; set; }
+        public string? ExceptionMessage { get; set; }
+        public bool IsException { get; set; }
+    }
+}

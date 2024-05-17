5db498b8a7280583942a2bb42f06c89894a8a9fa
sadyazz <97617659+sadyazz@users.noreply.github.com>
2024-05-17 16:15:19 +0200

    updated user-profile.component.ts

--- frontend/src/app/user-profile/user-profile.component.ts
+++ frontend/src/app/user-profile/user-profile.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {MyAuthService} from "../services/MyAuthService";
 import {Router} from "@angular/router";
 import {HttpClient} from "@angular/common/http";
-import {GetUserInfo, GetUserResponse} from "./get-user-info";
+import {MojConfig} from "../moj-config";
 
 @Component({
   selector: 'user-profile',
@@ -11,13 +11,29 @@ import {GetUserInfo, GetUserResponse} from "./get-user-info";
 })
 export class UserProfileComponent implements OnInit{
 
-  constructor(private myAuthService: MyAuthService, public router :Router, private httpClient: HttpClient) {
+  constructor(public myAuthService: MyAuthService, public router :Router, private httpClient: HttpClient) {
   }
 
+  ngOnInit(): void {
 
-  ngOnInit() {
+    }
 
+  logOut() {
 
+    let token = window.localStorage.getItem("my-auth-token")??"";
+    window.localStorage.setItem("my-auth-token","");
+
+    let url=MojConfig.adresa_servera+`/Autentifikacija/logout`
+    this.httpClient.post(url, {}, {
+      headers:{
+        "my-auth-token": token
+      }
+    }).subscribe(x=>{
+      console.log("logout uspjesan")
+    })
+
+    this.router.navigate(["/login"])
   }
+
 
 }

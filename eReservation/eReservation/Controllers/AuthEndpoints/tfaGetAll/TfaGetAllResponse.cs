d835bd959d7b312f3ec52347f8fb5d890faa89ff
sadyazz <97617659+sadyazz@users.noreply.github.com>
2024-05-23 18:11:50 +0200

    added a 2fa get all endpoint

---
+++ eReservation/eReservation/Controllers/AuthEndpoints/tfaGetAll/TfaGetAllResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace eReservation.Controllers.AuthEndpoints.tfaGetAll
+{
+    public class TfaGetAllResponse
+    {
+        public List<TfasGetAllResponseRow> Tfas { get; set; }
+    }
+
+    public class TfasGetAllResponseRow
+    {
+        public int ID { get; set; }
+        public string TwoFKey { get; set; }
+    }
+}

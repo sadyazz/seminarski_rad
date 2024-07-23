50d502791c303ac930da6b9f618666f43fb41a20
sadyazz <97617659+sadyazz@users.noreply.github.com>
2024-07-23 21:42:07 +0200

    finished 2fa page

--- frontend/src/app/endpoints/twofa-endpoint.ts
+++ frontend/src/app/endpoints/twofa-endpoint.ts
@@ -1,29 +1,32 @@
-import {BaseEndpoint} from "./base-endpoint";
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {MojConfig} from "../moj-config";
-import {Injectable} from "@angular/core";
-@Injectable({providedIn: 'root'})
-export class TfasGetallEndpoint implements BaseEndpoint<void, TfaGetAllResponse>{
-  constructor(public httpClient:HttpClient) { }
+import { BaseEndpoint } from "./base-endpoint";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { MojConfig } from "../moj-config";
+import { Injectable } from "@angular/core";
+
+@Injectable({ providedIn: 'root' })
+export class TfasGetallEndpoint implements BaseEndpoint<void, TfaGetAllResponse> {
+  constructor(public httpClient: HttpClient) { }
 
   Handle(request: void): Observable<TfaGetAllResponse> {
-    let url = MojConfig.adresa_servera + 'Tfa-GetAll';
+    let url = MojConfig.adresa_servera + '/Tfa-GetAll';
+    console.log('Request URL:', url); // Log the URL to the console
 
-    let token = window.localStorage.getItem("my-auth-token")??"";
+    let token = window.localStorage.getItem("my-auth-token") ?? "";
 
-    return this.httpClient.get<TfaGetAllResponse>(url , {
-      headers:{
+    return this.httpClient.get<TfaGetAllResponse>(url, {
+      headers: {
         "my-auth-token": token
       }
     });
   }
 }
-export interface TfaGetAllResponse{
-  tfas : TfaGetAllResponseTfa[];
+
+export interface TfaGetAllResponse {
+  tfas: TfaGetAllResponseTfa[];
 }
 
 export interface TfaGetAllResponseTfa {
-  id: number
-  twoFKey: string
+  id: number;
+  twoFKey: string;
 }

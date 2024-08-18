e9300d68f41966f92dd07b6269155ac96050c1c8
sadyazz <97617659+sadyazz@users.noreply.github.com>
2024-08-18 19:59:13 +0200

    implemented data fetching for user profile

--- frontend/src/app/user-profile/user-profile.component.ts
+++ frontend/src/app/user-profile/user-profile.component.ts
@@ -3,6 +3,7 @@ import {MyAuthService} from "../services/MyAuthService";
 import {Router} from "@angular/router";
 import {HttpClient} from "@angular/common/http";
 import {MojConfig} from "../moj-config";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'user-profile',
@@ -14,9 +15,35 @@ export class UserProfileComponent implements OnInit{
   constructor(public myAuthService: MyAuthService, public router :Router, private httpClient: HttpClient) {
   }
 
-  ngOnInit(): void {
+  user: any;
+  isEditing: boolean = false;
 
+  toggleEditMode() {
+    this.isEditing = !this.isEditing;
+  }
+  ngOnInit(): void {
+    const userId = this.myAuthService.returnId();
+    if (userId) {
+      this.getUserData(userId).subscribe(data => {
+        this.user = data;
+        this.tempUser = { ...data };
+        console.log(this.user);
+      }, error => {
+        console.error('Error fetching user data', error);
+      });
+    } else {
+      console.error('User ID not found');
     }
+    }
+
+  getUserData(id: number): Observable<any> {
+    const userId = this.myAuthService.returnId();
+    let url = MojConfig.adresa_servera+`/GetUserById?id=${userId}`;
+    if (userId) {
+      return this.httpClient.get<any>(url);
+    }
+    throw new Error('User ID not found');
+  }
 
   logOut() {
 
@@ -34,6 +61,22 @@ export class UserProfileComponent implements OnInit{
 
     this.router.navigate(["/login"])
   }
+  nazad(){
+    this.router.navigate(["/home"]);
+  }
+  tempUser: any;
+  saveChanges() {
+    const userId = this.myAuthService.returnId();
+    console.log("USER ID: ", userId);
+    const url = MojConfig.adresa_servera + `/EditUser?id=${userId}`;
 
-
+    this.httpClient.put(url, this.user).subscribe(
+      (response: any) => {
+        console.log('User updated successfully');
+      },
+      (error) => {
+        console.error('Error updating user data', error);
+      }
+    );
+  }
 }

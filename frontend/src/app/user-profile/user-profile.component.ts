e800b0403e6e895ea09819536b87eaa6efb54dd1
sadyazz <97617659+sadyazz@users.noreply.github.com>
2024-08-24 23:12:38 +0200

    added reservations and reviews to the user-profile component

--- frontend/src/app/user-profile/user-profile.component.ts
+++ frontend/src/app/user-profile/user-profile.component.ts
@@ -4,6 +4,7 @@ import {Router} from "@angular/router";
 import {HttpClient} from "@angular/common/http";
 import {MojConfig} from "../moj-config";
 import {Observable} from "rxjs";
+import {PropertiesGetAllResponse} from "../home/properties-getall-response";
 
 @Component({
   selector: 'user-profile',
@@ -17,6 +18,13 @@ export class UserProfileComponent implements OnInit{
 
   user: any;
   isEditing: boolean = false;
+  selectedFile = null;
+  properties:PropertiesGetAllResponse[] = [];
+  reviews:any;
+
+  onFileSelected(event:any) {
+      this.selectedFile = event.target.files[0];
+  }
 
   toggleEditMode() {
     this.isEditing = !this.isEditing;
@@ -27,14 +35,31 @@ export class UserProfileComponent implements OnInit{
       this.getUserData(userId).subscribe(data => {
         this.user = data;
         this.tempUser = { ...data };
-        console.log(this.user);
+        //console.log(this.user);
       }, error => {
         console.error('Error fetching user data', error);
       });
     } else {
       console.error('User ID not found');
     }
+    this.getReviews();
     }
+
+  getReviews(): void {
+    const userId = this.myAuthService.returnId();
+    let url = MojConfig.adresa_servera+`/GetReviewByUserId?userId=${userId}`;
+    this.httpClient.get<any[]>(url).subscribe(
+      (data) => {
+        this.reviews = data;
+        console.log("reviews -> ", data);
+      },
+      (error) => {
+        console.error('Error fetching reviews:', error);
+      }
+    );
+  }
+
+
 
   getUserData(id: number): Observable<any> {
     const userId = this.myAuthService.returnId();
@@ -61,6 +86,10 @@ export class UserProfileComponent implements OnInit{
 
     this.router.navigate(["/login"])
   }
+
+  scroll(el: HTMLElement) {
+    el.scrollIntoView({behavior: 'smooth'});
+  }
   nazad(){
     this.router.navigate(["/home"]);
   }
@@ -78,5 +107,20 @@ export class UserProfileComponent implements OnInit{
         console.error('Error updating user data', error);
       }
     );
+  }
+
+  getReservations() {
+    let url = MojConfig.adresa_servera + '/api/Properties/GetAll';
+    console.log('Fetching properties from URL:', url);
+
+    this.httpClient.get<PropertiesGetAllResponse[]>(url).subscribe(
+      (x: PropertiesGetAllResponse[]) => {
+        this.properties = x;
+      },
+      error => {
+        console.error('Error fetching properties', error);
+      }
+    );
+
   }
 }
